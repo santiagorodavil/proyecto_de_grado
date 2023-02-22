84e2834bc9a40ec81c83849847b3218a62de42be
santiagorodavil <s.rodrigueza@uniandes.edu.co>
2023-02-22 02:13:25 -0500

    update: recorre todas las fotos de cualquier dataset y muestra o la mascara o la region detectada

--- prueba_space_color.py
+++ prueba_space_color.py
@@ -3,27 +3,29 @@ import os
 import numpy as np
 
 GENERAL_PATH = os.getcwd()
-img = cv2.imread(GENERAL_PATH+"/Dataset/cafe_buenoEI/DSCN0404.JPG")
-lab_img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+for file in os.listdir(GENERAL_PATH+"/Dataset/cafe_maloEI/"):
+    print(file)
+    img = cv2.imread(GENERAL_PATH+"/Dataset/cafe_maloEI/"+file)
+    lab_img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
 
-# Red mask
-lower_red = np.array([0, 120, 0])
-upper_red = np.array([100, 200, 150])
-mask = cv2.inRange(lab_img, lower_red, upper_red)
+    # Red mask
+    lower_red = np.array([0, 120, 0])
+    upper_red = np.array([100, 200, 150])
+    mask = cv2.inRange(lab_img, lower_red, upper_red)
 
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-result = cv2.bitwise_and(img, img, mask=mask)
-contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-for c in contours:
-    area = cv2.contourArea(c)
-    if area > 70000:
-        cv2.drawContours(img, [c], -1, (0, 255, 0), 5)
-        print(area)
-result = cv2.resize(result, (400, 300))
-img = cv2.resize(img,(400,300))
-cv2.imshow("prueba", result)
-cv2.imshow("foto con bordes",img)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    result = cv2.bitwise_and(img, img, mask=mask)
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for c in contours:
+        area = cv2.contourArea(c)
+        if area > 70000:
+            cv2.drawContours(img, [c], -1, (0, 255, 0), 5)
+            print(area)
+    result = cv2.resize(result, (400, 300))
+    img = cv2.resize(img,(400,300))
+    #cv2.imshow("prueba"+file, result)
+    cv2.imshow("foto con bordes" + file,img)
 cv2.waitKey(0)
 
 
